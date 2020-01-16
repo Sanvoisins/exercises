69170be857134c020b644c4e0a67cb54289b68d5
Antoine SANVOISIN <46890505+Sanvoisins@users.noreply.github.com>
2020-01-16 16:28:00 +0100

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const uniqueChar = require ('./ex01/main')
-const stringReverse = require ('./ex02/main')
-const isAnagram = require('./ex03/main')
-
-// uniqueChar('ssd')
-// stringReverse('zoo')
-// isAnagram('hello','olleh')
